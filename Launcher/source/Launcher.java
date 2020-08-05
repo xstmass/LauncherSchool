4b5159f6f79c2a5d34d78c90225d557b7d9dde1d
KeeperJerry <keeperjerry@yandex.ru>
2020-08-05 15:21:25 +0300

    Relese 1.5.1
    Скорее hotfix для зеркала...
    Ниче особо не поменялось

--- Launcher/source/Launcher.java
+++ Launcher/source/Launcher.java
@@ -59,7 +59,7 @@ public final class Launcher
 {
     // Version info
     @LauncherAPI
-    public static final String VERSION = "1.5.0";
+    public static final String VERSION = "1.5.1";
     @LauncherAPI
     public static final String BUILD = readBuildNumber();
     @LauncherAPI
