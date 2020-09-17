b5912d0af2c8dd6b813fbc934b1be7432989b6e9
KeeperJerry <keeperjerry@yandex.ru>
2020-09-17 21:28:20 +0300

    Релиз 1.6.0 короче
    Нахер этот forge, я там от либ прихуел...

--- Launcher/source/Launcher.java
+++ Launcher/source/Launcher.java
@@ -59,7 +59,7 @@ public final class Launcher
 {
     // Version info
     @LauncherAPI
-    public static final String VERSION = "1.5.2";
+    public static final String VERSION = "1.6.0";
     @LauncherAPI
     public static final String BUILD = readBuildNumber();
     @LauncherAPI
