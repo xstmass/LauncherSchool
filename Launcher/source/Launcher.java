d61c4e0785014d0c6083e0c8d5d082495ec0b2db
KeeperJerry <keeperjerry@yandex.ru>
2020-12-09 11:22:44 +0300

    Пока кину это в DEV
    Надо перебросить людей на зеркало, иначе будет заеб с обновлением v2...
    1.16.4 пока не готов

--- Launcher/source/Launcher.java
+++ Launcher/source/Launcher.java
@@ -59,7 +59,7 @@ public final class Launcher
 {
     // Version info
     @LauncherAPI
-    public static final String VERSION = "1.6.0";
+    public static final String VERSION = "1.6.1";
     @LauncherAPI
     public static final String BUILD = readBuildNumber();
     @LauncherAPI
