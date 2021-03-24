585b87ef88f64f52e9d6b3806586c3c13b56ddda
KeeperJerry <keeperjerry@yandex.ru>
2021-03-24 03:05:32 +0300

    Relese 1.7.2.0

--- Launcher/source/Launcher.java
+++ Launcher/source/Launcher.java
@@ -59,7 +59,7 @@ public final class Launcher
 {
     // Version info
     @LauncherAPI
-    public static final String VERSION = "1.7.1.1";
+    public static final String VERSION = "1.7.2.0";
     @LauncherAPI
     public static final String BUILD = readBuildNumber();
     @LauncherAPI
