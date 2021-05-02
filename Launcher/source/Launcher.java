5bb6932e2cc198ee3ade38255449c114a28c693c
KeeperJerry <keeperjerry@yandex.ru>
2021-05-02 08:34:28 +0300

    Release 1.7.3.0
    Документация уже опубликована

--- Launcher/source/Launcher.java
+++ Launcher/source/Launcher.java
@@ -59,7 +59,7 @@ public final class Launcher
 {
     // Version info
     @LauncherAPI
-    public static final String VERSION = "1.7.2.4";
+    public static final String VERSION = "1.7.3.0";
     @LauncherAPI
     public static final String BUILD = readBuildNumber();
     @LauncherAPI
