f63a11aeb8519eb538a70e9bfb673483206ae93e
KeeperJerry <keeperjerry@yandex.ru>
2022-01-10 00:07:20 +0300

    Чистим и возвращаем
    Откат + убедился, что переменные не нужны

--- Launcher/source-authlib/yggdrasil/YggdrasilUserApiService.java
+++ Launcher/source-authlib/yggdrasil/YggdrasilUserApiService.java
@@ -2,17 +2,12 @@ package com.mojang.authlib.yggdrasil;
 
 import com.google.common.collect.ImmutableSet;
 import com.mojang.authlib.Environment;
-import com.mojang.authlib.HttpAuthenticationService;
 import com.mojang.authlib.exceptions.AuthenticationException;
 import com.mojang.authlib.minecraft.TelemetrySession;
 import com.mojang.authlib.minecraft.UserApiService;
-import com.mojang.authlib.minecraft.client.MinecraftClient;
-import com.mojang.authlib.yggdrasil.response.BlockListResponse;
-import com.mojang.authlib.yggdrasil.response.UserAttributesResponse;
 
 import javax.annotation.Nullable;
 import java.net.Proxy;
-import java.net.URL;
 import java.time.Instant;
 import java.util.Set;
 import java.util.UUID;
@@ -22,11 +17,6 @@ public class YggdrasilUserApiService implements UserApiService {
     private static final long BLOCKLIST_REQUEST_COOLDOWN_SECONDS = 120;
     private static final UUID ZERO_UUID = new UUID(0, 0);
 
-//    private final URL routePrivileges;
-//    private final URL routeBlocklist;
-//
-//    private final MinecraftClient minecraftClient;
-//    private final Environment environment;
     private UserProperties properties = OFFLINE_PROPERTIES;
 
     @Nullable
