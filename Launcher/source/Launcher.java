869d3dcf5a848ace3355e27426501a9a69bd7402
KeeperJerry <keeperjerry@yandex.ru>
2022-02-03 00:47:18 +0300

    Release 1.7.5.2
    Поторопился с выпуском релиза, потому что сделал hotfix для authlib систем.
    И это еще не все...

--- Launcher/source/Launcher.java
+++ Launcher/source/Launcher.java
@@ -59,7 +59,7 @@ public final class Launcher
 {
     // Version info
     @LauncherAPI
-    public static final String VERSION = "1.7.5.1";
+    public static final String VERSION = "1.7.5.2";
     @LauncherAPI
     public static final String BUILD = readBuildNumber();
     @LauncherAPI
